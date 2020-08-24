45a304c87e6e3042a5095bfd3ee3961c314ad5c0
Laine <39713102+lainie-ftw@users.noreply.github.com>
2020-08-24 13:28:04 -0400

    Update SampleCamelRouter.java

--- camel-kafka-demo/src/main/java/com/sample/camel/route/SampleCamelRouter.java
+++ camel-kafka-demo/src/main/java/com/sample/camel/route/SampleCamelRouter.java
@@ -46,7 +46,7 @@ public class SampleCamelRouter extends RouteBuilder {
    from("direct:goodbye")
         .routeId("log-goodbye")
         .log(LoggingLevel.INFO, "Goodbye World!!!!")
-        .transform().simple('{"text":"from goodbye endpoint in running application"}')
+        .transform().simple("{'text':'from goodbye endpoint in running application'}")
         .to("kafka:serverless-demo");
     }
 
